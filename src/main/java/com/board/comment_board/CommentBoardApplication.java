e499c0d95e44da42d9f8ff9becd2b0b550c2e080
임채성 <pan1129@naver.com>
2022-07-08 22:30:00 +0900

    게시판 댓글기능 프로젝트 commit

---
+++ src/main/java/com/board/comment_board/CommentBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.comment_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/comment_board/CommentBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.comment_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
