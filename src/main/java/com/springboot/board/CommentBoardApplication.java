1da850b6555af730c2e069adfb62eeda7e116e93
임채성 <pan1129@naver.com>
2022-07-09 21:59:36 +0900

    Hello Controller, lombok 테스트 코드 Commit

--- src/main/java/com/springboot/board/CommentBoardApplication.java
+++ src/main/java/com/springboot/board/CommentBoardApplication.java
@@ -1,4 +1,4 @@
-package com.board.comment_board;
+package com.springboot.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
