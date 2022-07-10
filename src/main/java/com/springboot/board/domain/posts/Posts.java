658aca01b97f1d44ab1d4860b2a718fa39812d1d
임채성 <pan1129@naver.com>
2022-07-10 18:59:52 +0900

    게시글 저장후 조회 테스트 Commit

---
+++ src/main/java/com/springboot/board/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.springboot.board.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String email;
+
+    @Builder
+    public Posts(String title, String content, String email){
+        this.title = title;
+        this.content = content;
+        this.email = email;
+    }
+
+}
