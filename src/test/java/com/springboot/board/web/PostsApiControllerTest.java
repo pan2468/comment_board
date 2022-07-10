e6bb51eed696025b1c892a1afec87cb70ba1ece4
임채성 <pan1129@naver.com>
2022-07-10 23:18:03 +0900

    게시글 조회 테스트 Commit

--- src/test/java/com/springboot/board/web/PostsApiControllerTest.java
+++ src/test/java/com/springboot/board/web/PostsApiControllerTest.java
@@ -3,7 +3,6 @@ package com.springboot.board.web;
 import com.springboot.board.domain.posts.Posts;
 import com.springboot.board.domain.posts.PostsRepository;
 import com.springboot.board.web.dto.PostsSaveRequestDto;
-import com.springboot.board.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,6 +32,29 @@ public class PostsApiControllerTest {
     public void tearDown()throws Exception{
         postsRepository.deleteAll();
     }
+
+    @Test
+    public void Posts_게시글조회(){
+        //given
+        String title = "제목 조회";
+        String content = "내용 조회";
+        String email = "이메일 조회";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .email(email)
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getEmail()).isEqualTo(email);
+    }
+
 
     @Test
     public void Posts_저장하기(){
@@ -62,28 +83,27 @@ public class PostsApiControllerTest {
     }
 
     @Test
-    public void Posts_수정하기(){
+    public void Posts_수정하기()throws Exception{
         //given
-        String title = "제목 수정하기";
-        String content = "내용 수정하기";
+        Posts posts = new Posts();
+        List<Posts> postsUpdate = postsRepository.findAllById(posts.getId());
 
-        PostsUpdateRequestDto requestUpdateDto = PostsUpdateRequestDto.builder()
-                .title(title)
-                .content(content)
-                .build();
 
-        String url = "http://localhost:" + port + "/api/v1/posts";
+
+
+    }
+
+    @Test
+
+    public void Posts_상세(){
+        //given
+
+//        Order savedOrder = orderRepository.findById(order.getId())
+//                .orElseThrow(EntityNotFoundException::new);
 
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestUpdateDto, Long.class);
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
-        List<Posts> all = postsRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(title);
-        assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 }
