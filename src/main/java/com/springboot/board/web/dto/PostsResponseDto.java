a9f867645aa3636f66e7471564082bb298d2f25c
임채성 <pan1129@naver.com>
2022-07-26 01:37:50 +0900

    게시글 수정과 수정버튼 오류해결 Commit

--- src/main/java/com/springboot/board/web/dto/PostsResponseDto.java
+++ src/main/java/com/springboot/board/web/dto/PostsResponseDto.java
@@ -9,12 +9,14 @@ public class PostsResponseDto {
     private Long id;
     private String title;
     private String content;
+    private String author;
     private String email;
 
     public PostsResponseDto(Posts entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
+        this.author = entity.getAuthor();
         this.email = entity.getEmail();
     }
 
