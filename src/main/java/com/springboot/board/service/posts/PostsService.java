ccb3df95b0945a224f8e91af95ea1d6cbc251e11
임채성 <pan1129@naver.com>
2022-07-26 16:28:39 +0900

    게시글 트랜잭션 선언하여 PUT 개선 commit

--- src/main/java/com/springboot/board/service/posts/PostsService.java
+++ src/main/java/com/springboot/board/service/posts/PostsService.java
@@ -24,6 +24,7 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
